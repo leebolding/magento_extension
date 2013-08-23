1d0278e30ecd2c36bf3ac66d994e4e6cc20cd18b
liorkuyer <liorkuyer@gmail.com>
2013-08-23 12:39:58 -0700

    copy script

--- copy.sh
+++ copy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 TARGET=$1;
+DIRECTION=$2;
+
 DIRS="app/code/local/TVPage/ app/design/adminhtml/default/default/template/tvpconnect/";
 FILES="app/etc/modules/TVPage_Connect.xml app/design/adminhtml/default/default/layout/tvpconnect.xml";
 
@@ -39,13 +41,25 @@ done
 # Start the copy
 for i in $DIRS
   do
-    echo "Copying $TARGET/$i $i";
-    cp -a "$TARGET/$i" "$i";
+    if [ "$DIRECTION" ]; then
+      echo "Copying $i $TARGET/$i";
+      cp -a "$i" "$TARGET/$i";
+    else 
+      echo "Copying $TARGET/$i $i";
+      cp -a "$TARGET/$i" "$i";
+    fi;
 done;
 
 # Start the copy
 for f in $FILES
   do
-    echo "Copying $TARGET/$f $f";
-    cp "$TARGET/$f" "$f";
+    if [ "$DIRECTION" ]; then
+      echo "Copying $TARGET/$f $f";
+      cp "$f" "$TARGET/$f";
+      
+    else 
+      echo "Copying $TARGET/$f $f";
+      cp "$TARGET/$f" "$f";
+    fi;
+
 done;
